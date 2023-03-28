372a8b14c8024e5225865ebe13901571517e951a
Isaacg1 <iag8@njit.edu>
2023-03-28 06:43:14 -0400

    added files

--- Part3HW/Server.java
+++ Part3HW/Server.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class Server {
     int port = 3001;
@@ -23,11 +24,11 @@ public class Server {
                 if (incoming_client != null) {
                     System.out.println("Client connected");
                     ServerThread sClient = new ServerThread(incoming_client, this);
-                    
+
                     clients.add(sClient);
                     sClient.start();
                     incoming_client = null;
-                    
+
                 }
             } while ((incoming_client = serverSocket.accept()) != null);
         } catch (IOException e) {
@@ -37,14 +38,15 @@ public class Server {
             System.out.println("closing server socket");
         }
     }
+
     protected synchronized void disconnect(ServerThread client) {
-		long id = client.getId();
+        long id = client.getId();
         client.disconnect();
-		broadcast("Disconnected", id);
-	}
-    
+        broadcast("Disconnected", id);
+    }
+
     protected synchronized void broadcast(String message, long id) {
-        if(processCommand(message, id)){
+        if (processCommand(message, id)) {
 
             return;
         }
@@ -53,7 +55,7 @@ public class Server {
         // change as clients connect/disconnect
         message = String.format("User[%d]: %s", id, message);
         // end temp identifier
-        
+
         // loop over clients and send out the message
         Iterator<ServerThread> it = clients.iterator();
         while (it.hasNext()) {
@@ -67,23 +69,47 @@ public class Server {
         }
     }
 
-    private boolean processCommand(String message, long clientId){
+    private boolean processCommand(String message, long clientId) {
         System.out.println("Checking command: " + message);
-        if(message.equalsIgnoreCase("disconnect")){
+        if (message.equalsIgnoreCase("disconnect")) {
             Iterator<ServerThread> it = clients.iterator();
             while (it.hasNext()) {
                 ServerThread client = it.next();
-                if(client.getId() == clientId){
+                if (client.getId() == clientId) {
                     it.remove();
                     disconnect(client);
-                    
+
                     break;
                 }
             }
             return true;
+            //Iag8 coing flip
+        } else if (message.equalsIgnoreCase("flip")) { //creates the command that the server looks for. For this example if the client inputs flip, it'll begin to run this code
+        broadcast("You flipped a coin and it landed on " + (new Random().nextBoolean() ? "heads" : "tails"), clientId); 
+        //This generates a random boolean value  using the nextBoolean(). If the boolean value is true, the result of the ternary operator is the string "heads".
+        //If the boolean value is false, the result is the string "tails".
+        return true;
+        } 
+        
+        //Iag Message Shuffle
+        else if (message.startsWith("randomize")) {
+            String[] parts = message.split("randomize");
+            if (parts.length > 1) {
+                String msg = parts[1];
+                String[] letters = msg.split("");
+                for (int i = 0; i < letters.length; i++) {
+                    String t = letters[i];
+                    int newIndex = (int) Math.floor(Math.random() * letters.length);
+                    letters[i] = letters[newIndex];
+                    letters[newIndex] = t;
+                }
+                broadcast(String.join("", letters), clientId);
+                return true;
+            }
         }
         return false;
     }
+
     public static void main(String[] args) {
         System.out.println("Starting Server");
         Server server = new Server();
